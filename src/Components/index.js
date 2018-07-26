4e3b7fdab62ddf6a75b4268c4cf030596d2d512f
Kevin Baugh <kbaugh@indeed.com>
2018-07-26 15:58:03 -0500

    Changed package name and better example names

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,11 +3,11 @@ import { Text, Input, Debug } from 'Primitives'
 
 export default class extends React.Component {
   state = {
-    someVal: ''
+    exampleValue: ''
   }
   handleChange = e => {
     this.setState({
-      someVal: e.currentTarget.value
+      exampleValue: e.currentTarget.value
     })
   }
 
@@ -16,10 +16,12 @@ export default class extends React.Component {
       <Text>Hello from the src/Components/index.js file!</Text>
       <Input
         placeholder='Type here...'
-        value={this.state.someVal}
+        value={this.state.exampleValue}
         onChange={this.handleChange}
       />
-      <p>{this.state.someVal || 'Type in the Input Box to change this text'}</p>
+      <p>
+        {this.state.exampleValue || 'Type in the Input Box to change this text'}
+      </p>
     </React.Fragment>
   )
 }
