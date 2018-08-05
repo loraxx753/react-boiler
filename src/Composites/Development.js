c03cbf5b83ae15bc8884ae3ca92e36bc76150ff6
Kevin Baugh <kbaugh@indeed.com>
2018-08-04 21:04:17 -0500

    Added some color api

--- src/Composites/Development.js
+++ src/Composites/Development.js
@@ -2,4 +2,13 @@ import React from 'react'
 import { Api } from 'Primitives'
 import { Debug } from 'Primitives'
 
-export default props => <Api path='/todos' />
+export default props => (
+  <Api colors path='id' params={{ hex: '24B1E0' }}>
+    {(data, response) => (
+      <React.Fragment>
+        <Debug>{response}</Debug>
+        <Debug>{data}</Debug>
+      </React.Fragment>
+    )}
+  </Api>
+)
--- src/Primitives/Api.js
+++ src/Primitives/Api.js
@@ -15,7 +15,8 @@ import { Debug } from 'Primitives'
 
 const urls = {
   default: 'https://jsonplaceholder.typicode.com/',
-  stocks: 'https://api.iextrading.com/1.0'
+  stocks: 'https://api.iextrading.com/1.0',
+  colors: 'http://www.thecolorapi.com/'
 }
 
 export default class extends React.Component {
@@ -24,11 +25,11 @@ export default class extends React.Component {
     const apiType = Object.keys(props).filter(
       key => Object.keys(urls).indexOf(key) > -1
     )
-    console.log(apiType)
     this.state = {
       axiosInstance: axios.create({
         baseURL: urls[apiType] || urls.default,
-        timeout: 2000
+        timeout: 2000,
+        crossdomain: true
         //   headers: { 'X-Custom-Header': 'foobar' }
       }),
 
@@ -46,11 +47,12 @@ export default class extends React.Component {
   render () {
     return (
       <AxiosProvider instance={this.state.axiosInstance}>
-        <Get url={this.props.path}>
+        <Get url={this.props.path} {...this.props}>
           {(error, response, isLoading, onReload) => {
             if (response !== null) {
-              if (this.props.children) return this.props.children(response)
-              else return <Debug>{response}</Debug>
+              const { data, ...rest } = response
+              if (this.props.children) return this.props.children(data, rest)
+              else return <Debug>{data}</Debug>
             }
             return <div>Default message before request is made.</div>
           }}
